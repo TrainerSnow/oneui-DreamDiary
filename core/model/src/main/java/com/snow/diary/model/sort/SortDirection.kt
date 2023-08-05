731c02e7859971d2e8f38b26a164b69f08564182
simon <simon.bela.bumiller@gmx.de>
2023-08-05 12:53:26 +0200

    Sort models

---
+++ core/model/src/main/java/com/snow/diary/model/sort/SortDirection.kt
@@ -0,0 +1,11 @@
+package com.snow.diary.model.sort
+
+enum class SortDirection {
+
+    Ascending,
+
+    Descending,
+
+    Unspecified
+
+}
---
+++ core/model/src/main/java/com/snow/diary/model/sort/SortMode.kt
@@ -0,0 +1,19 @@
+package com.snow.diary.model.sort;
+
+enum class SortMode {
+
+    Created,
+
+    Updated,
+
+    Alphabetically,
+
+    Happiness,
+
+    Clearness,
+
+    Relation,
+
+    Length
+
+}
