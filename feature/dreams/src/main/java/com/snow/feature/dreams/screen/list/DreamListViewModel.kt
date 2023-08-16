8006e857c8fae3170296db1c43d8fb53f4e802a7
simon <simon.bela.bumiller@gmx.de>
2023-08-16 20:46:57 +0200

    Screen for list dreams

---
+++ feature/dreams/src/main/java/com/snow/feature/dreams/screen/list/DreamListViewModel.kt
@@ -0,0 +1,12 @@
+package com.snow.feature.dreams.screen.list;
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+internal class DreamListViewModel @Inject constructor(
+
+): ViewModel() {
+
+}
