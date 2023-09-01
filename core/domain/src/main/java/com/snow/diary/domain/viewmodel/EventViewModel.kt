8579e8b6d2f2ef86b80c0d01ffcb34e1ce417333
simon <simon.bela.bumiller@gmx.de>
2023-09-01 18:23:53 +0200

    Event view model

---
+++ core/domain/src/main/java/com/snow/diary/domain/viewmodel/EventViewModel.kt
@@ -0,0 +1,30 @@
+package com.snow.diary.domain.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+
+
+/**
+ * Super class for any [ViewModel] that plans to receive events from the ui.
+ *
+ * @param Event
+ */
+abstract class EventViewModel <Event>: ViewModel() {
+
+    private val events = MutableSharedFlow<Event>()
+
+    init {
+        viewModelScope.launch {
+            events.collect(::handleEvent)
+        }
+    }
+
+    fun onEvent(event: Event) = viewModelScope.launch {
+        events.emit(event)
+    }
+
+    protected abstract suspend fun handleEvent(event: Event)
+
+}
