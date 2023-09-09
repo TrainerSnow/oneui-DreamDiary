d32e659d123794dda05f17c1daa27f719f22bc32
simon <simon.bela.bumiller@gmx.de>
2023-09-09 22:37:55 +0200

    person list navigation

---
+++ feature/persons/src/main/java/com/snow/diary/persons/nav/PersonListNavigation.kt
@@ -0,0 +1,33 @@
+package com.snow.diary.persons.nav
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.snow.diary.model.data.Person
+import com.snow.diary.model.data.Relation
+import com.snow.diary.persons.screen.list.PersonList
+
+private const val personListRoute = "person_list"
+
+fun NavController.goToPersonList(navOptions: NavOptions? = null) = navigate(personListRoute, navOptions)
+
+fun NavGraphBuilder.personList(
+    onNavigateBack: () -> Unit,
+    onAddPerson: () -> Unit,
+    onSearchPerson: () -> Unit,
+    onRelationClick: (Relation) -> Unit,
+    onPersonClick: (Person) -> Unit
+) {
+    composable(
+        route = personListRoute
+    ) {
+        PersonList(
+            onNavigateBack = onNavigateBack,
+            onAddPerson = onAddPerson,
+            onSearchPerson = onSearchPerson,
+            onRelationClick = onRelationClick,
+            onPersonClick = onPersonClick
+        )
+    }
+}
