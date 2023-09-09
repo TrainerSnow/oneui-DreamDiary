560c5898a4a42d69a433d146331da9056f3d1743
simon <simon.bela.bumiller@gmx.de>
2023-09-09 21:34:12 +0200

    Add todo, don't handle export

--- feature/export/src/main/java/com/snow/diary/export/screen/ExportViewModel.kt
+++ feature/export/src/main/java/com/snow/diary/export/screen/ExportViewModel.kt
@@ -1,8 +1,5 @@
 package com.snow.diary.export.screen;
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.os.Environment
 import androidx.lifecycle.viewModelScope
 import com.snow.diary.domain.action.cross.AllDreamLocationCrossrefs
 import com.snow.diary.domain.action.cross.AllDreamPersonCrossrefs
@@ -11,12 +8,9 @@ import com.snow.diary.domain.action.location.AllLocations
 import com.snow.diary.domain.action.person.AllPersons
 import com.snow.diary.domain.action.relation.AllRelations
 import com.snow.diary.domain.viewmodel.EventViewModel
-import com.snow.diary.export.R
 import com.snow.diary.io.ExportFiletype
 import com.snow.diary.io.data.IOData
-import com.snow.diary.io.exporting.IExportAdapter
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -24,14 +18,10 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.io.File
-import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
 internal class ExportViewModel @Inject constructor(
-    @SuppressLint("StaticFieldLeak")
-    @ApplicationContext val context: Context,
     val allDreams: AllDreams,
     val allPersons: AllPersons,
     val allLocations: AllLocations,
@@ -43,21 +33,9 @@ internal class ExportViewModel @Inject constructor(
     private val _state = MutableStateFlow(ExportState())
     val state: StateFlow<ExportState> = _state
 
-    @Suppress("IMPLICIT_CAST_TO_ANY")
     override suspend fun handleEvent(event: ExportEvent) = when (event) {
-        ExportEvent.Export -> handleExport()
+        ExportEvent.Export -> TODO()
         is ExportEvent.SelectFiletype -> handleFiletypeChange(event.filetype)
-    }
-
-    private suspend fun handleExport() {
-        val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-        val fileName = context.getString(R.string.export_file_name, LocalDateTime.now().toString())
-
-        val file = File(dir, fileName)
-        val os = file.outputStream()
-        val data = getData()
-        val exporter = IExportAdapter.getInstance(state.value.selectedFiletype)
-        exporter.export(data, os)
     }
 
     private suspend fun getData(): IOData = withContext(Dispatchers.IO) {
