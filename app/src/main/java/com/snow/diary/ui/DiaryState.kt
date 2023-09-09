403604d3cde1d85dd7490d736a4f0c8c2f737198
simon <simon.bela.bumiller@gmx.de>
2023-09-09 22:03:29 +0200

    app root with nav drawer

--- app/src/main/java/com/snow/diary/ui/DiaryState.kt
+++ app/src/main/java/com/snow/diary/ui/DiaryState.kt
@@ -1,16 +1,48 @@
 package com.snow.diary.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.snow.diary.nav.TopLevelDestinations
+import com.snow.feature.dreams.nav.goToDreamList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.oneui.compose.layout.internal.SlidingDrawerState
+import org.oneui.compose.layout.internal.rememberSlidingDrawerState
 
 data class DiaryState(
 
-    val navController: NavHostController
+    val navController: NavHostController,
 
-)
+    val drawerState: SlidingDrawerState,
+
+    val scope: CoroutineScope
+
+) {
+
+    var currentNavDest by mutableStateOf(TopLevelDestinations.Dreams)
+
+    fun navigateTo(navDest: TopLevelDestinations) {
+        when (navDest) {
+            TopLevelDestinations.Dreams -> navController.goToDreamList()
+            TopLevelDestinations.Persons -> TODO()
+            TopLevelDestinations.Locations -> TODO()
+            TopLevelDestinations.Statistics -> TODO()
+        }
+        currentNavDest = navDest
+    }
+
+    fun openDrawer() = scope.launch { drawerState.openAnimate() }
+
+}
 
 @Composable
 fun rememberDiaryState(
-    navController: NavHostController = rememberNavController()
-) = DiaryState(navController)
+    navController: NavHostController = rememberNavController(),
+    drawerState: SlidingDrawerState = rememberSlidingDrawerState(),
+    scope: CoroutineScope = rememberCoroutineScope()
+) = DiaryState(navController, drawerState, scope)
