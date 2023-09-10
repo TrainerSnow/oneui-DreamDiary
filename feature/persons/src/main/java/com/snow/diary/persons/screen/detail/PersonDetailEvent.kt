133cbc132b077c98000739adbf65b8f9eb55ece9
simon <simon.bela.bumiller@gmx.de>
2023-09-10 08:17:16 +0200

    person detail screen

---
+++ feature/persons/src/main/java/com/snow/diary/persons/screen/detail/PersonDetailEvent.kt
@@ -0,0 +1,15 @@
+package com.snow.diary.persons.screen.detail;
+
+import com.snow.diary.model.data.Dream
+
+internal sealed class PersonDetailEvent {
+
+    data class DreamFavouriteClick(
+        val dream: Dream
+    ): PersonDetailEvent()
+
+    data class ChangeTab(
+        val tab: PersonDetailTab
+    ): PersonDetailEvent()
+
+}
--- feature/persons/src/main/java/com/snow/diary/persons/screen/detail/PersonDetailTab.kt
+++ feature/persons/src/main/java/com/snow/diary/persons/screen/detail/PersonDetailTab.kt
@@ -1,5 +1,9 @@
 package com.snow.diary.persons.screen.detail
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.snow.diary.persons.R
+
 enum class PersonDetailTab {
 
     General,
@@ -7,3 +11,12 @@ enum class PersonDetailTab {
     Dreams
 
 }
+
+
+@Composable
+fun PersonDetailTab.localizedName() = stringResource(
+    when (this) {
+        PersonDetailTab.General -> R.string.person_detail_loading
+        PersonDetailTab.Dreams -> R.string.person_detail_dreams
+    }
+)
