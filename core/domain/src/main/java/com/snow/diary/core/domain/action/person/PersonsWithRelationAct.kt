c84467b8453e6abc85b4f0bf340a2851061e49a0
simon <simon.bela.bumiller@gmx.de>
2023-09-13 09:49:47 +0200

    weird ewrror

--- core/domain/src/main/java/com/snow/diary/core/domain/action/person/PersonsWithRelationAct.kt
+++ core/domain/src/main/java/com/snow/diary/core/domain/action/person/PersonsWithRelationAct.kt
@@ -2,16 +2,19 @@ package com.snow.diary.core.domain.action.person;
 
 import com.snow.diary.core.database.dao.PersonDao
 import com.snow.diary.core.domain.action.FlowAction
-import com.snow.diary.core.domain.pure.mapToModel
 import com.snow.diary.core.model.combine.PersonWithRelations
 import com.snow.diary.core.model.data.Person
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class PersonsWithRelationsAct(
     val personDao: PersonDao
 ) : FlowAction<Person, List<PersonWithRelations>>() {
     override fun Person.createFlow(): Flow<List<PersonWithRelations>> = personDao
         .getAllPersonsWithRelationsById()
-        .mapToModel()
-
+        .map {
+            it.map {
+                it.toModel()
+            }
+        }
 }
