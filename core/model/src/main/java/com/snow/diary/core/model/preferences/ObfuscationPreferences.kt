514ec47c697ed17b211b9f6e26465523949ac451
simon <simon.bela.bumiller@gmx.de>
2023-09-14 20:50:52 +0200

    preferences models

---
+++ core/model/src/main/java/com/snow/diary/core/model/preferences/ObfuscationPreferences.kt
@@ -0,0 +1,15 @@
+package com.snow.diary.core.model.preferences;
+
+data class ObfuscationPreferences (
+
+    val obfuscationEnabled: Boolean,
+
+    val obfuscateDreams: Boolean,
+
+    val obfuscatePersons: Boolean,
+
+    val obfuscateLocations: Boolean,
+
+    val obfuscateRelations: Boolean
+
+)
