6b478a4d803d7fca479976017f1d8f75631e1d80
simon <simon.bela.bumiller@gmx.de>
2023-09-15 22:18:11 +0200

    progress indicator while obfuscating

--- feature/preferences/src/main/java/com/snow/diary/feature/preferences/screen/obfuscation/ObfuscatePreferencesViewModel.kt
+++ feature/preferences/src/main/java/com/snow/diary/feature/preferences/screen/obfuscation/ObfuscatePreferencesViewModel.kt
@@ -1,12 +1,16 @@
 package com.snow.diary.feature.preferences.screen.obfuscation;
 
 import androidx.lifecycle.viewModelScope
+import com.snow.diary.core.domain.action.obfuscation.Deobfuscate
+import com.snow.diary.core.domain.action.obfuscation.Obfuscate
 import com.snow.diary.core.domain.action.preferences.GetPreferences
 import com.snow.diary.core.domain.action.preferences.UpdateObfuscationPreferences
 import com.snow.diary.core.domain.viewmodel.EventViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.withContext
@@ -15,7 +19,9 @@ import javax.inject.Inject
 @HiltViewModel
 class ObfuscatePreferencesViewModel @Inject constructor(
     getPreferences: GetPreferences,
-    val updateObfuscationPreferences: UpdateObfuscationPreferences
+    val updateObfuscationPreferences: UpdateObfuscationPreferences,
+    val obfuscate: Obfuscate,
+    val deobfuscate: Deobfuscate
 ) : EventViewModel<ObfuscationPreferencesEvent>() {
 
     val preferences = getPreferences(Unit)
@@ -26,24 +32,39 @@ class ObfuscatePreferencesViewModel @Inject constructor(
             initialValue = null
         )
 
+    private val _isObfuscating = MutableStateFlow(false)
+    val isObfuscating: StateFlow<Boolean> = _isObfuscating
+
     override suspend fun handleEvent(event: ObfuscationPreferencesEvent) =
         withContext(Dispatchers.IO) {
-
             val prefs = preferences.value ?: return@withContext
 
             when (event) {
-                is ObfuscationPreferencesEvent.ChangeDoObfuscation -> updateObfuscationPreferences(
-                    prefs.copy(obfuscationEnabled = event.enabled)
-                )
+                is ObfuscationPreferencesEvent.ChangeDoObfuscation -> {
+                    val newPrefs = prefs.copy(obfuscationEnabled = event.enabled)
+                    updateObfuscationPreferences(newPrefs)
+                    _isObfuscating.emit(true)
+                    if (event.enabled) {
+                        obfuscate(newPrefs)
+                    }
+                    else {
+                        deobfuscate(newPrefs)
+                    }
+                    _isObfuscating.emit(false)
+                }
+
                 is ObfuscationPreferencesEvent.ChangeObfuscateDreams -> updateObfuscationPreferences(
                     prefs.copy(obfuscateDreams = event.obfuscate)
                 )
+
                 is ObfuscationPreferencesEvent.ChangeObfuscateLocations -> updateObfuscationPreferences(
                     prefs.copy(obfuscateLocations = event.obfuscate)
                 )
+
                 is ObfuscationPreferencesEvent.ChangeObfuscatePersons -> updateObfuscationPreferences(
                     prefs.copy(obfuscatePersons = event.obfuscate)
                 )
+
                 is ObfuscationPreferencesEvent.ChangeObfuscateRelations -> updateObfuscationPreferences(
                     prefs.copy(obfuscateRelations = event.obfuscate)
                 )
