84af40fa24602190382480be21e2daa7d42ddacb
simon <simon.bela.bumiller@gmx.de>
2023-09-16 16:22:03 +0200

    config stuff for

--- app/src/main/java/com/snow/diary/RootActivity.kt
+++ app/src/main/java/com/snow/diary/RootActivity.kt
@@ -3,11 +3,16 @@ package com.snow.diary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.snow.diary.core.domain.action.preferences.GetPreferences
 import com.snow.diary.ui.DiaryApplicationRoot
 import com.snow.diary.ui.rememberDiaryState
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class RootActivity : ComponentActivity() {
@@ -23,7 +28,14 @@ class RootActivity : ComponentActivity() {
                 getPreferences = getPreferences
             )
 
-            DiaryApplicationRoot(diaryState)
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                DiaryApplicationRoot(diaryState)
+            }
         }
     }
 
