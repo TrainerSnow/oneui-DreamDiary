811fab18b105be11a66d1725392f90721de4e943
simon <simon.bela.bumiller@gmx.de>
2023-09-19 22:21:10 +0200

    Utils for line graph labeling

---
+++ core/ui/src/main/java/com/snow/diary/core/ui/graph/line/AxisLineConfig.kt
@@ -0,0 +1,17 @@
+package com.snow.diary.core.ui.graph.line;
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeCap
+
+data class AxisLineConfig (
+
+    val color: Color,
+
+    val strokeWidth: Float = 1F,
+
+    val pathEffect: PathEffect? = null,
+
+    val strokeCap: StrokeCap = StrokeCap.Butt
+
+)
