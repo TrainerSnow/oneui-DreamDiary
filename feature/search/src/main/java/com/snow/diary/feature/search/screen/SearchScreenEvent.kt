ea49135393b55fb1e3c0a642d9f59d30d5b5330d
simon <simon.bela.bumiller@gmx.de>
2023-09-30 22:45:44 +0200

    search event and state

---
+++ feature/search/src/main/java/com/snow/diary/feature/search/screen/SearchScreenEvent.kt
@@ -0,0 +1,31 @@
+package com.snow.diary.feature.search.screen;
+
+import com.snow.diary.core.model.data.Dream
+import com.snow.diary.core.model.data.Person
+import com.snow.diary.core.ui.component.StatisticsDateRanges
+
+internal sealed class SearchEvent {
+
+    data class ChangeQuery(
+        val query: String
+    ): SearchEvent()
+
+    data class ChangeTab(
+        val tab: SearchTabs
+    ): SearchEvent()
+
+    data object ToggleRangeDialog: SearchEvent()
+
+    data class ChangeRange(
+        val range: StatisticsDateRanges
+    ): SearchEvent()
+
+    data class DreamFavouriteClick(
+        val dream: Dream
+    ): SearchEvent()
+
+    data class PersonFavouriteClick(
+        val person: Person
+    ): SearchEvent()
+
+}
