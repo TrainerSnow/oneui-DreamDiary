68b72974ecae41ffd7c09ff27fbd5fae2e7e5150
simon <simon.bela.bumiller@gmx.de>
2023-10-03 10:37:43 +0200

    fix wrong api call

--- core/io/src/main/java/com/snow/diary/core/io/importing/impl/JSONImportAdapter.kt
+++ core/io/src/main/java/com/snow/diary/core/io/importing/impl/JSONImportAdapter.kt
@@ -4,11 +4,14 @@ import com.snow.diary.core.io.data.IOData
 import com.snow.diary.core.io.exporting.impl.JSONIOData
 import com.snow.diary.core.io.importing.IImportAdapter
 import kotlinx.serialization.json.Json
+import java.io.BufferedReader
 import java.io.InputStream
 
 class JSONImportAdapter: IImportAdapter {
     override fun import(`is`: InputStream): IOData {
-        val jsonString = `is`.readAllBytes().decodeToString()
+        val reader = BufferedReader(`is`.reader())
+        val jsonString = reader.readText()
+        reader.close()
 
         val obj = Json.decodeFromString<JSONIOData>(jsonString)
         return obj.toIOData()
