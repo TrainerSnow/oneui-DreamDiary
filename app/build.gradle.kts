b98ab16bb4f9df3539f055a1f86ef9a4e3f37ffb
simon <simon.bela.bumiller@gmx.de>
2023-10-10 20:35:20 +0200

    build first release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
