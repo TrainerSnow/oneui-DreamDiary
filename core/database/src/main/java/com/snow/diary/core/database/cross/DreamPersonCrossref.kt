37ab8320e8866a55d8fa42ca33cf29044089e07f
simon <simon.bela.bumiller@gmx.de>
2023-10-12 20:06:28 +0200

    ensure CASCADE is used for crossrefs

--- core/database/src/main/java/com/snow/diary/core/database/cross/DreamPersonCrossref.kt
+++ core/database/src/main/java/com/snow/diary/core/database/cross/DreamPersonCrossref.kt
@@ -2,10 +2,27 @@ package com.snow.diary.core.database.cross
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import com.snow.diary.core.database.model.DreamEntity
+import com.snow.diary.core.database.model.PersonEntity
 
 @Entity(
     tableName = "dream_person_crossref",
-    primaryKeys = ["dreamId", "personId"]
+    primaryKeys = ["dreamId", "personId"],
+    foreignKeys = [
+        ForeignKey(
+            entity = DreamEntity::class,
+            parentColumns = ["dreamId"],
+            childColumns = ["dreamId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = PersonEntity::class,
+            parentColumns = ["personId"],
+            childColumns = ["personId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
 )
 data class DreamPersonCrossref(
 
--- core/database/src/main/java/com/snow/diary/core/database/cross/PersonRelationCrossref.kt
+++ core/database/src/main/java/com/snow/diary/core/database/cross/PersonRelationCrossref.kt
@@ -2,10 +2,27 @@ package com.snow.diary.core.database.cross
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import com.snow.diary.core.database.model.PersonEntity
+import com.snow.diary.core.database.model.RelationEntity
 
 @Entity(
     tableName = "person_relation_crossref",
-    primaryKeys = ["personId", "relationId"]
+    primaryKeys = ["personId", "relationId"],
+    foreignKeys = [
+        ForeignKey(
+            entity = PersonEntity::class,
+            parentColumns = ["personId"],
+            childColumns = ["personId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = RelationEntity::class,
+            parentColumns = ["relationId"],
+            childColumns = ["relationId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
 )
 data class PersonRelationCrossref(
 
